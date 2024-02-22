55c3e01f89833e0010da00afe455c92ab6e7b2c7
wafflefrank <83715021+wafflefrank@users.noreply.github.com>
2024-02-22 19:47:54 +0800

    Updated

--- src/views/Loby.vue
+++ src/views/Loby.vue
@@ -42,7 +42,7 @@
   <!-- 帳戶 -->
   <div class="d-flex align-items-center justify-content-center mb-5 mt-5 phone_style">
     <!-- <span class="text-white me-5 fs-4">帳戶: {{ this.memberAccount }}</span> -->
-    <span class="text-dark me-5 text-start"
+    <span style="color: rgb(77, 74, 74)" class=" me-5 text-start"
       ><span class="fs-4"
         >剩餘抽獎次數: <span class="drawNums_style">{{ this.drawNum }}</span></span
       >
@@ -91,7 +91,7 @@
           </div>
           <div class="prize-background">
             <div>
-              <i v-for="item in 9" class=" fa-solid fa-snowflake" :key="item"></i>
+              <i v-for="item in 9" class="fa-solid fa-snowflake" :key="item"></i>
             </div>
           </div>
         </div>
@@ -105,14 +105,14 @@
         <!-- <p>1.為避免【會員瀏覽器問題】所有得獎者獎項一律以LEO娛樂城抽獎系統歷史記錄為主。</p>
         <p>2.中獎禮金將於21號後依序轉入會員的遊戲帳戶。</p>
         <p>3.本活動最終解釋權歸LEO娛樂城所有。</p> -->
-        <pre>{{ this.awardText }}</pre>
+        <pre style="color: rgb(77, 74, 74)">{{ this.awardText }}</pre>
       </div>
     </div>
     <!-- 活動說明 -->
     <div class="directionBox">
       <div class="D_title"><img src="../assets/banner/act_des.png" alt="" /></div>
       <div class="D_Word">
-        <pre>{{ this.activityText }}</pre>
+        <pre style="color: rgb(77, 74, 74)">{{ this.activityText }}</pre>
         <!-- <p>1.LEO娛樂城EX與KU經銷專屬幸運大抽獎，僅有EX與KU旗下會員才可參加抽獎活動，非EX與KU旗下會員將不符合資格。</p>
         <p>2.會員當月存款金額累計滿10000點(含)以上且需一倍流水，獲得抽獎次數1次，最多儲值滿30000點(含)以上且需一倍流水，獲得抽獎次數3次。</p>
         <p>3.次月1號至10號經審查合格後，將於隔日11號可獲得抽獎序號，會員需自行至抽獎活動網址進行抽獎。</p>
